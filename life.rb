07ad53e067ca26cf0967e79f31910b335ae740cb
zendesk-sunlee <sun.lee@zendesk.com>
2022-01-07 16:18:54 +1100

    Add all 4 rules

--- life.rb
+++ life.rb
@@ -33,9 +33,27 @@ puts number_of_neighbors(block, 1,1) # => 1
 def is_alive?(board, x, y)
   # Any live cell with fewer than two live neighbours dies, as if by underpopulation.
   if was_alive?(board, x,y) && number_of_neighbors(board, x, y) < 2
+    puts "rule 1"
     return false
-  end
+  elsif was_alive?(board, x,y) && (number_of_neighbors(board, x, y) == 2 || number_of_neighbors(board, x, y) == 3)
+    puts "rule 2"
+    return true
+  elsif was_alive?(board, x,y) && number_of_neighbors(board, x, y) > 3
+    puts "rule 3"
+    return false
+  elsif !was_alive?(board, x,y) && number_of_neighbors(board, x, y) == 3
+    puts "rule 4"
+    return true
+  else
+    raise "Somehow we did not match any rules"
+  end 
 end
 
 blinker = Set[{x:2, y: 2},{x:2, y: 3},{x:2, y: 1}]
-puts is_alive?(blinker, 2, 1) # => false
+puts is_alive?(blinker, 2, 1) # => false # Rule 1
+puts is_alive?(blinker, 2, 2) # => true # Rule 2
+puts is_alive?(blinker, 1, 2) # => true # Rule 4
+
+
+block_with_nub = Set[{x:2, y: 2},{x:3, y: 3},{x:3, y: 2},{x:2, y: 3}, {x:1, y:1}]
+puts is_alive?(block_with_nub, 2, 2) # => false # Rule 3
