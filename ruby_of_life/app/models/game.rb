78e5598d8d527cc01dd26140b50701212bf0ea04
zendesk-sunlee <sun.lee@zendesk.com>
2022-03-18 16:59:29 +1100

    Removed redundant method

--- ruby_of_life/app/models/game.rb
+++ ruby_of_life/app/models/game.rb
@@ -1,23 +1,10 @@
 class Game < ApplicationRecord
   has_many :cells
 
-  $counts = {}
-  def count(name)
-    if $counts[name]
-      $counts[name] += 1
-    else
-      $counts[name] = 1
-    end
-  end
-
-  def self.reset
-    $counts = {}
-  end
-
-  def was_alive?(cell)
-    count("was_alive?")
+  def was_alive?(coord)
+    x,y = coord
     self.cells
-    .map {|c| c.x == cell.x && c.y == cell.y }
+    .map {|c| c.x == x && c.y == y }
     .reduce(false) {|s, n| s or n}
   end
 
@@ -28,33 +15,17 @@ class Game < ApplicationRecord
     .reduce({}) {|a, v| if a[v] then a[v] += 1 else a[v] = 1 end; a }
   end
 
-  def is_alive?(cell, number_of_neighbors_hash )
-    count("is_alive?")
-    result = case number_of_neighbors_hash[cell.coords]
-    when 2 then was_alive?(cell)
-    when 3 then true
-    else false
-    end
-    return result
-  end
-
-  # Rather than saving, instead initialize a new game and cells. Return them.
   def step
-    number_of_neighbors_hash = get_number_of_neighbors_hash
     Game.new(
       name: self.name,
-      cells: self.cells
-      .flat_map {|c| c.neighbors.to_a }
-      .uniq{|c| [c.x, c.y]}
-      .flat_map {|c| is_alive?(c,number_of_neighbors_hash) ? [c] : [] }
-      .uniq{|c| [c.x, c.y]}
+      cells: get_number_of_neighbors_hash
+      .flat_map do |k, v|
+        if v == 2 && was_alive?(k) || v== 3
+          [Cell.pnew(k.first, k.second)]
+        else
+          []
+        end
+      end
     )
-  end
-end
-
-class Object
-  def teel
-    pp self.length
-    self
   end
 end
